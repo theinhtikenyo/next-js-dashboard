511e85b79d1089dd70d3d6d3df88527710c4e22f
Thein <theinhtitenyo@gmail.com>
2025-06-04 13:23:37 +0630

    dashboard data

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,25 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+import { fetchCardData} from '../lib/data';
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
 
-// 	return data;
-// }
+async function listInvoices() {
+  const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+  return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+    const invoices = await fetchCardData();
+    return Response.json(invoices);
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
 }
